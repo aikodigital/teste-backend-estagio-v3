e7b843f7c40b16fa574db4d821272e56be03acd8
RafaelPinheiroLimaCoder <rafaelpinheirolima.coder@gmail.com>
2023-03-28 18:33:22 -0300

    Upload SpringBoot - parametros

---
+++ src/test/java/com/aikodigital/teste_estagio_backend_v3/TesteEstagioBackendV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aikodigital.teste_estagio_backend_v3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteEstagioBackendV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
